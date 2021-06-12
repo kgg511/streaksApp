c6944851bb4ab4e9bf0260606d1e219349d34e4c
kgg511 <67660463+kgg511@users.noreply.github.com>
2021-06-12 15:13:23 -0400

    Worked in TasksPage & streak.dart. TaskPage: added streaks list which will hold each streak object, which is really a row. I embedded the column within another column so that I could display the list of rows objects and the button. Not 100% confident if my logic is correct, but hoping to test soon

--- lib/screens/TasksPage.dart
+++ lib/screens/TasksPage.dart
@@ -7,6 +7,7 @@ import 'package:streaksApp/screens/results_page.dart';
 import 'package:streaksApp/components/bottom_button.dart';
 import 'package:streaksApp/components/round_icon_button.dart';
 import 'package:streaksApp/calculator_brain.dart';
+import 'package:streaksApp/streak.dart';
 
 
 class TasksPage extends StatefulWidget {
@@ -16,6 +17,8 @@ class TasksPage extends StatefulWidget {
 }
 
 class _TasksPageState extends State<TasksPage> {
+  List<streak> streaks = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,17 +26,39 @@ class _TasksPageState extends State<TasksPage> {
         title: Text('MY HABITS'),
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
+        children: [//**in order to display a list within a list, I embedded the column within another column
           BottomButton(
             buttonTitle: 'Add Habit',
             onTap: () {
-
-              //navigator.push
+              //collects data
+              setState(() {
+                streaks.add(streak(name: , number: ,));
+              });
             },
+          ),
+
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: streaks, //the list of streak objects, each of which returns a row
           ),
         ],
       ),
     );
   }
 }
+
+
+
+//<Widget>[
+//
+//          BottomButton(
+//            buttonTitle: 'Add Habit',
+//            onTap: () {
+//              //navigator.push
+//              //collects data
+//              setState(() {
+//                streaks.add(streak(name: , number: ,));
+//              });
+//            },
+//          ),
+//        ],
--- lib/streak.dart
+++ lib/streak.dart
@@ -1,6 +1,8 @@
 
 //when button is clicked and data is submitted, data should create a streak object
 
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 
@@ -8,20 +10,20 @@ class streak extends StatelessWidget {
 
   streak({this.name, this.number}){}
   //Color color;
-  String name; //name of the streak
+  int name; //name of the streak
   String number;//number of days they have continued the streak
 
   //it should return a row
-  //the instance variables should be displayed within widgets. Use methods
-  //text, text, button to continue streak
+
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Row(
         children: <Widget>[
-          Text(number),
-          Text(name),
+          Text(number),//just testing the row widget. Still need to add button
+          Text(name.toString()),
+
         ],
       ),
     );
